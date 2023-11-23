1d436f53d2aaba7e309b1bb386ad4d5e7de934dc
Weszybz <127601118+Weszybz@users.noreply.github.com>
2023-11-23 18:54:23 +0000

    Maintenance for Food (1): - Improved variable names for better readability ('i' to 'image', 'l' to 'isAvailable').

--- src/example/Food.java
+++ src/example/Food.java
@@ -2,15 +2,13 @@ package example;
 
 import java.awt.Graphics;
 import java.util.Random;
-import java.awt.Graphics2D;
-import java.awt.Image;
 
 
 /**
  * The Food class represents the food object that the snake can eat in the game.
  * It extends the SnakeObject class from the MyFrame class.
  */
-public class Food extends MyFrame.SnakeObject
+public class Food extends GameFrame.SnakeObject
 {
 	/** A unique serial identification*/
 	private static final long serialVersionUID = -3641221053272056036L;
@@ -21,30 +19,30 @@ public class Food extends MyFrame.SnakeObject
 	 * Intiialises the food properties
 	 */
 	public Food()	{
-		this.l = true;
+		this.isAvailable = true;
 
 		// Set the image based on a random number
-		this.i = ImageUtil.images.get(String.valueOf(new Random().nextInt(10)));
+		this.image = ImageUtil.images.get(String.valueOf(new Random().nextInt(10)));
 
-		this.w = i.getWidth(null);
-		this.h = i.getHeight(null);
+		this.width = image.getWidth(null);
+		this.height = image.getHeight(null);
 
 		// Randomly set the position within the game window.
-		this.x = (int) (Math.random() * (870 - w + 10));
-		this.y = (int) (Math.random() * (560 - h - 40));
+		this.x = (int) (Math.random() * (870 - width + 10));
+		this.y = (int) (Math.random() * (560 - height - 40));
 	}
 
 	/**
 	 * Checks if the food has been eaten by the snake and updates the game state accordingly.
 	 *
-	 * @param mySnake represents the snake in the game.
+	 * @param snakeGame represents the snake in the game.
 	 */
-	public void eaten(MyFrame.MySnake mySnake)	{
+	public void eaten(GameFrame.SnakeGame snakeGame)	{
 		// Mark the food as eaten, increase the snake length, and update the score.
-		if (mySnake.getRectangle().intersects(this.getRectangle()) && l && mySnake.l)		{
-			this.l = false;
-			mySnake.changeLength(mySnake.getLength() + 1);
-			mySnake.score += 521;
+		if (snakeGame.getRectangle().intersects(this.getRectangle()) && isAvailable && snakeGame.isAvailable)		{
+			this.isAvailable = false;
+			snakeGame.changeLength(snakeGame.getLength() + 1);
+			snakeGame.score += 521;
 		}
 	}
 
@@ -55,6 +53,6 @@ public class Food extends MyFrame.SnakeObject
 	@Override
 	public void draw(Graphics g)
 	{
-		g.drawImage(i, x, y, null);
+		g.drawImage(image, x, y, null);
 	}
 }
