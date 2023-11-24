37ab59820c23b5b22b70e304ffe87c37230435a0
Weszybz <127601118+Weszybz@users.noreply.github.com>
2023-11-24 11:53:54 +0000

    Maintenance for ImageUtil (2): - Removing unused code - Renamed the food obstacle images with numerical suffixes from "0" to "16". - Used a for loop instead of listing each image.

--- src/example/ImageUtil.java
+++ src/example/ImageUtil.java
@@ -21,23 +21,10 @@ public class ImageUtil
 		images.put("snake-body", GameUtil.getImage("example/snake-body.png"));
 
 		// Obstacles images
-		images.put("0", GameUtil.getImage("example/food-kiwi.png"));
-		images.put("1", GameUtil.getImage("example/food-lemon.png"));
-		images.put("2", GameUtil.getImage("example/food-litchi.png"));
-		images.put("3", GameUtil.getImage("example/food-mango.png"));
-		images.put("4", GameUtil.getImage("example/food-apple.png"));
-		images.put("5", GameUtil.getImage("example/food-banana.png"));
-		images.put("6", GameUtil.getImage("example/food-blueberry.png"));
-		images.put("7", GameUtil.getImage("example/food-cherry.png"));
-		images.put("8", GameUtil.getImage("example/food-durian.png"));
-		images.put("9", GameUtil.getImage("example/food-grape.png"));
-		images.put("10", GameUtil.getImage("example/food-grapefruit.png"));
-		images.put("11", GameUtil.getImage("example/food-peach.png"));
-		images.put("12", GameUtil.getImage("example/food-pear.png"));
-		images.put("13", GameUtil.getImage("example/food-orange.png"));
-		images.put("14", GameUtil.getImage("example/food-pineapple.png"));
-		images.put("15", GameUtil.getImage("example/food-strawberry.png"));
-		images.put("16", GameUtil.getImage("example/food-watermelon.png"));
+		for (int i = 0; i <= 17; i++) {
+			images.put(String.valueOf(i), GameUtil.getImage("example/food-" + i + ".png"));
+		}
+
 		// User interface and scene background images
 		images.put("UI-background", GameUtil.getImage("example/UI-background.png"));
 		images.put("game-scene-01", GameUtil.getImage("example/game-scene-01.jpg"));
